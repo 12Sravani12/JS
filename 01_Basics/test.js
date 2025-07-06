af9de9187b8705e891a13d6b3d13af73f70326f5
Sravani Thigulla <141737977+12Sravani12@users.noreply.github.com>
2025-07-06 16:27:55 +0000

    container created

---
+++ 01_Basics/test.js
@@ -0,0 +1,9 @@
+//what ever the extension(type of data) of the file might be .txt or .js both are called as source files only.
+//the difference between .js and .txt is that which file we have to inject into to a software and execute
+//there are  some softwares(compilers or interpretes) will have the capability of understanding the syntax
+console.log("HEllo World")// Save the file and run
+/*Now set the Git environment:
+1.login
+2.create repository
+3.create codespace
+4.got to view->command pallet(ctrl+shift+P)*/
